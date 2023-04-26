0bfd3c1cb3977e2a7232390541ca392609972366
Cristiano de Sousa <cristiano.renuncia@gmail.com>
2023-04-26 15:18:38 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/Program.cs
@@ -0,0 +1,11 @@
+﻿using bytebank;
+
+ContaCorrente contaDoCristiano = new ContaCorrente();
+
+    contaDoCristiano.titular = "Cristiano Sousa";
+    contaDoCristiano.numero_agencia = 15;
+    contaDoCristiano.conta = "1010x";
+    contaDoCristiano.saldo = 100;
+
+Console.WriteLine("Saldo da conta do Cristiano " + contaDoCristiano.saldo);
+
