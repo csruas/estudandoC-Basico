9eebf7195148bbac72227d75fc9a13b529f96bc4
Cristiano de Sousa <cristiano.renuncia@gmail.com>
2023-04-27 18:15:42 -0300

    Inserindo comportamento de uma classe

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -13,5 +13,44 @@ namespace bytebank
         public string titular;
         public double saldo;
 
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+           
+        }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+
+            if(this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.Sacar(valor);
+                destino.Depositar(valor);
+                return true;
+            }
+
+        }
+
+
     }
+
+   
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -9,3 +9,32 @@ ContaCorrente contaDoCristiano = new ContaCorrente();
 
 Console.WriteLine("Saldo da conta do Cristiano " + contaDoCristiano.saldo);
 
+ContaCorrente contaDaClaudia = new ContaCorrente();
+contaDaClaudia.titular = "Cláudia Vidal";
+contaDaClaudia.numero_agencia = 16;
+contaDaClaudia.conta = "22xxx-x";
+contaDaClaudia.saldo = 350;
+
+contaDoCristiano.Depositar(100);
+
+Console.WriteLine("Saldo da conta do Cristiano depois de depositar " + contaDoCristiano.saldo);
+
+
+
+if(contaDoCristiano.Sacar(200) == true)
+{
+    Console.WriteLine("Voce sacou  " + contaDoCristiano.saldo);
+}
+else
+{
+    Console.WriteLine("Voce não tem saldo suficiente. " );
+}
+
+Console.WriteLine("Transferindo....");
+contaDoCristiano.Transferir(50, contaDaClaudia);
+
+Console.WriteLine("Saldo da conta do Cristiano " + contaDoCristiano.saldo);
+Console.WriteLine("Saldo da conta do Cládia " + contaDaClaudia.saldo);
+
+
+
